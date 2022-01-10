21f1cb539c2a36c0b8b1288785977751d64d03b9
Sierrawastaken <josiah.mosetter@gmail.com>
2022-01-09 22:28:30 -0800

    changed file structure to be more modular

--- AsukaBot.js
+++ AsukaBot.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js")
-require("dotenv").config()
-const Token = process.env.TOKEN
-const Prefix = "a!"
-
 const generateImage = require("./dependencies/generateImage")
+const config = require("./config.json")
+const fs = require("fs")
 
 const client = new Discord.Client({
     intents:[
@@ -12,6 +10,25 @@ const client = new Discord.Client({
         "GUILD_MEMBERS"
     ]
 })
+
+client.config = config;
+client.commands = new Discord.Collection()
+
+const events = fs.readdirSync("./events").filter(file => file.endsWith(".js"))
+for (const file of events) {
+    const eventName = file.split(".")[0];
+    const event = require(`./events/${file}`);
+    client.on(eventName, event.bind(null, client));
+}
+
+const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for (const file of commands) {
+  const commandName = file.split(".")[0];
+  const command = require(`./commands/${file}`);
+
+  console.log(`Attempting to load command ${commandName}`);
+  client.commands.set(command.name, command);
+}
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}`)
@@ -26,12 +43,9 @@ client.on("messageCreate", (message) => {
 })
 
 //listens for members joining and welcomes them
-
-const WelcomeChannelId = "929850220811022366"
-
 client.on("guildMemberAdd", async (member) => {
     const img = await generateImage(member)
-    member.guild.channels.cache.get(WelcomeChannelId).send({
+    member.guild.channels.cache.get(config.WelcomeChannelId).send({
         content: `<@${member.id}> Welcome to the server!`,
         files: [img]
     })
@@ -40,14 +54,16 @@ client.on("guildMemberAdd", async (member) => {
 //listen for msgs
 client.on("message", async msg =>{
     //ignore msgs that dont start with the prefix and parse the msg
-    if (!msg.content.startsWith(Prefix)) return
-    const args = msg.content.slice(Prefix.length).split(/ +/)
+    if (!msg.content.startsWith(config.prefix) || msg.author.bot ) return
+    const args = msg.content.slice(config.prefix.length).split(/ +/)
     const command = args.shift().toLocaleLowerCase()
 
+    
     //ping command, just replies with "pong"
     if (command === "ping") {
         msg.reply("pong")
     }
+    
 
     // membercount command
     if (command === "membercount") {
@@ -60,5 +76,10 @@ client.on("message", async msg =>{
         await message.react(`✔`) 
         await message.react(`❌`)
     }
+    if (command === "say") {
+        let text = args.join(" ")
+        msg.delete()
+        msg.channel.send(text)
+    }
 })
-client.login(Token)
+client.login(config.token)
---
+++ events/messageCreate.js
@@ -0,0 +1,17 @@
+module.exports = (client, message) => {
+   
+    //make sure command starts with the correct prefix
+     if (message.content.indexOf(client.config.prefix) !== 0) return;
+    
+    2 //ignore bots
+    if (message.author.bot) return;
+  
+    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+  
+    const cmd = client.commands.get(command);
+  
+    if (!cmd) return;
+  
+    cmd.run(client, message, args);
+  };
